bafb133a624de1d0b81dc1cc4baca3e178f6e559
DanilaBurmistrov <Burmistrov.danila2016@yandex.ru>
2021-10-02 12:44:17 +0300

    обновил функции

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-function getRandomFromRange(min, max, number) {
-  return (Math.random() * (max - min) + min).toFixed(number);
+function getRandomFloatFromRange(min, max, numberAfterComma) {
+  if (max <= min) {
+    min = 0;
+  }
+  return Math.random() * (max - min) + min.toFixed(numberAfterComma);
 }
 
-getRandomFromRange(0, 10, 5);
+getRandomFloatFromRange(0, 10, 5);
 
 function getRandomIntFromRange(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  if (max <= min) {
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 getRandomIntFromRange(0, 10);
+
+

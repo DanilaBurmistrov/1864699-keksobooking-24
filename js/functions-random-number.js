1e5d9a2cb92205b519674749d7f66b89d3d55626
DanilaBurmistrov <Burmistrov.danila2016@yandex.ru>
2021-10-11 18:22:49 +0300

    Создал модульный код

---
+++ js/functions-random-number.js
@@ -0,0 +1,26 @@
+function getRandomNumberInRange (from, to) {
+  if (to <= from) {
+    throw new Error('ошибка, максимальное значение не может быть меньше минимального');
+  }
+  if (from < 0 || to < 0) {
+    throw new Error('ошибка, значения не могут быть отрицательными числами');
+  }
+  return Math.random() * (to - from + 1) + from;
+}
+
+function getRandomFloatFromRange(min, max, numberAfterComma) {
+  return getRandomNumberInRange(min, max).toFixed(numberAfterComma);
+}
+
+
+function getRandomIntFromRange(min, max) {
+  return Math.floor(getRandomNumberInRange(min, max));
+}
+
+
+function getRandomElementOfArray (randomElement) {
+  const calculatedElement = randomElement[getRandomNumberInRange (0, randomElement.length - 1)];
+  return calculatedElement;
+}
+
+export {getRandomIntFromRange, getRandomFloatFromRange, getRandomElementOfArray};
--- js/main.js
+++ js/main.js
@@ -1,81 +1,23 @@
-const RANDOM_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const RANDOM_CHECKINS = ['12:00', '13:00', '14:00'];
-const RANDOM_CHECKOUTS = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-const AVATARS = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
-const MAX_USER_ADS = 10;
-const MIN_PRICE = 1000;
-const MAX_PRICE = 10000;
-const MIN_ROOMS = 1;
-const MAX_ROOMS = 5;
-const MIN_GUESTS = 1;
-const MAX_GUESTS = 5;
-const AVATAR_BASE_URL = 'img/avatars/';
-const AVATAR_FORMAT = 'png';
+import {RANDOM_TYPES,
+  RANDOM_CHECKINS,
+  RANDOM_CHECKOUTS,
+  FEATURES,
+  PHOTOS,
+  MAX_USER_ADS,
+  MIN_PRICE,
+  MAX_PRICE,
+  MIN_ROOMS,
+  MAX_ROOMS,
+  MIN_GUESTS,
+  MAX_GUESTS} from './constants.js';
 
+import {getAvatarUrl} from './functions-random-avatar.js';
 
-function getRandomNumberInRange (from, to) {
-  if (to <= from) {
-    throw new Error('ошибка, максимальное значение не может быть меньше минимального');
-  }
-  if (from < 0 || to < 0) {
-    throw new Error('ошибка, значения не могут быть отрицательными числами');
-  }
-  return Math.random() * (to - from + 1) + from;
-}
+import {getRandomArrayFromArray} from './functions-random-array.js';
 
-
-function getRandomFloatFromRange(min, max, numberAfterComma) {
-  return getRandomNumberInRange(min, max).toFixed(numberAfterComma);
-}
-
-
-function getRandomIntFromRange(min, max) {
-  return Math.floor(getRandomNumberInRange(min, max));
-}
-
-
-function getRandomElementOfArray (randomElement) {
-  const calculatedElement = randomElement[getRandomNumberInRange (0, randomElement.length - 1)];
-  return calculatedElement;
-}
-
-function getRandomArrayFromArray(transmittedArray) {
-  const maxLength = transmittedArray.length;
-  const lengthOfArray = getRandomIntFromRange(1, maxLength);
-  const array = [];
-
-  while (array.length < lengthOfArray) {
-    const indexOfElement = getRandomIntFromRange(0, maxLength - 1);
-    const element = transmittedArray[indexOfElement];
-
-    if (!array.includes(element)) {
-      array.push(element);
-    }
-  }
-  return array;
-}
-
-const takenAvatars = [];
-
-function getAvatarUrl() {
-  function getRandomAvatar() {
-    let found = false;
-    while (!found) {
-      const key = getRandomIntFromRange(0, AVATARS.length - 1);
-      if (!takenAvatars.includes(AVATARS[key])) {
-        takenAvatars.push(AVATARS[key]);
-        found = true;
-        return AVATARS[key];
-      }
-    }
-  }
-  const avatarUrl = `${AVATAR_BASE_URL}user${getRandomAvatar()}.${AVATAR_FORMAT}`;
-  return avatarUrl;
-}
+import {getRandomIntFromRange,
+  getRandomFloatFromRange,
+  getRandomElementOfArray} from './functions-random-number.js';
 
 function generateDataArray () {
   const resthouses  = [];

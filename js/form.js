a7db876dd42f6e17ec3055435fb3d323802424f1
DanilaBurmistrov <Burmistrov.danila2016@yandex.ru>
2021-10-17 21:09:10 +0300

    Создал блок с формами

---
+++ js/form.js
@@ -0,0 +1,22 @@
+const formsAttributes = document.querySelector('.ad-form');
+const mapsAttributes = document.querySelector('.map__filters');
+const Fs =  document.querySelectorAll('fieldset');
+const Sd = document.querySelectorAll('selected');
+
+const generateStatusOfOptions = function () {
+  formsAttributes.classList.add('ad-form--disabled');
+  mapsAttributes.classList.add('map__filters--disabled');
+  Fs.setAttribute('disabled');
+  Sd.setAttribute('disabled');
+};
+
+generateStatusOfOptions();
+
+const generateStatusOfWindow = function () {
+  formsAttributes.classList.remove('ad-form--disabled');
+  mapsAttributes.classList.remove('map__filters--disabled');
+  Fs.removeAttribute('disabled');
+  Sd.removeAttribute('disabled');
+};
+generateStatusOfWindow();
+

1dfc9252c48e3042afa5cfa6b46601a0c4e34b01
DanilaBurmistrov <Burmistrov.danila2016@yandex.ru>
2021-10-30 14:18:05 +0300

    внес правки в валидацию

--- js/form.js
+++ js/form.js
@@ -26,3 +26,54 @@ const toggleFormState = (isDisabled) => {
 };
 
 export {toggleFormState};
+
+const adForm = document.querySelector('.ad-form');
+const inputTitle = adForm.querySelector('#title');
+
+const minTitleLength = inputTitle.minLength;
+const maxTitleLength = inputTitle.maxLength;
+
+export const getAllowedValueOfTitle = () => {
+  const valueLength = inputTitle.value.length;
+
+  if (valueLength < minTitleLength) {
+    inputTitle.setCustomValidity(`Минимальная длина — 30 символов, ещё ${minTitleLength - valueLength} символов`);
+  } else if (valueLength > maxTitleLength) {
+    inputTitle.setCustomValidity(`Максимальная длина — 100 символов, удалите лишние ${valueLength - maxTitleLength} символов`);
+  } else {
+    inputTitle.setCustomValidity('');
+  }
+  inputTitle.reportValidity();
+};
+
+inputTitle.addEventListener('input', getAllowedValueOfTitle);
+
+const numberOfRoomsAdForm = adForm.querySelector('#room_number');
+const capacityOfRoomsAdForm = adForm.querySelector('#capacity');
+
+export const getValidateCapacityAndRooms = () => {
+  const rooms = Number(numberOfRoomsAdForm.value);
+  const guests = Number(capacityOfRoomsAdForm.value);
+  if (rooms === 100 && guests !== 0) {
+    numberOfRoomsAdForm.setCustomValidity('Такое колличество комнат не для гостей');
+  } else if (rooms !== 100 && guests === 0) {
+    numberOfRoomsAdForm.setCustomValidity('Не для гостей" можно выбрать только 100 комнат');
+  } else if (rooms < guests) {
+    numberOfRoomsAdForm.setCustomValidity('Колличество комнат должно быть больше или равно колличеству гостей');
+  } else {
+    numberOfRoomsAdForm.setCustomValidity('');
+  }
+  numberOfRoomsAdForm.reportValidity();
+};
+
+numberOfRoomsAdForm.addEventListener('change', getValidateCapacityAndRooms);
+capacityOfRoomsAdForm.addEventListener('change', getValidateCapacityAndRooms);
+
+export const generateValidityError = function (evt) {
+  const field = evt.target;
+  if (!field.validity.valid) {
+    field.style.outline = '2px solid red';
+  } else {
+    field.style = '';
+  }
+};

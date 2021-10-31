3dd927a95e65152aa62c5d99c515a3df56bc8cef
DanilaBurmistrov <Burmistrov.danila2016@yandex.ru>
2021-10-31 17:09:21 +0300

    внес правки в валидацию3

--- js/form.js
+++ js/form.js
@@ -1,31 +1,34 @@
-// const formsAttributes = document.querySelector('.ad-form');
-// const mapsAttributes = document.querySelector('.map__filters');
-// const fieldsetElements = document.querySelectorAll('fieldset');
-// const selectElements = document.querySelectorAll('select');
+import {MAX_APARTMENTS,
+  MIN_VISITORS} from './constants.js';
 
-// const toggleFormState = (isDisabled) => {
-//   if (isDisabled) {
-//     formsAttributes.classList.add('ad-form--disabled');
-//     mapsAttributes.classList.add('map__filters--disabled');
-//     fieldsetElements.forEach ((fieldsetElement) => {
-//       fieldsetElement.setAttribute('disabled', true);
-//     });
-//     selectElements.forEach ((selectElement) => {
-//       selectElement.setAttribute('disabled', true);
-//     });
-//   } else {
-//     formsAttributes.classList.remove('ad-form--disabled');
-//     mapsAttributes.classList.remove('map__filters--disabled');
-//     fieldsetElements.forEach ((fieldsetElement) => {
-//       fieldsetElement.removeAttribute('disabled', false);
-//     });
-//     selectElements.forEach ((selectElement) => {
-//       selectElement.removeAttribute('disabled', false);
-//     });
-//   }
-// };
+const formsAttributes = document.querySelector('.ad-form');
+const mapsAttributes = document.querySelector('.map__filters');
+const fieldsetElements = document.querySelectorAll('fieldset');
+const selectElements = document.querySelectorAll('select');
 
-// export {toggleFormState};
+const toggleFormState = (isDisabled) => {
+  if (isDisabled) {
+    formsAttributes.classList.add('ad-form--disabled');
+    mapsAttributes.classList.add('map__filters--disabled');
+    fieldsetElements.forEach ((fieldsetElement) => {
+      fieldsetElement.setAttribute('disabled', true);
+    });
+    selectElements.forEach ((selectElement) => {
+      selectElement.setAttribute('disabled', true);
+    });
+  } else {
+    formsAttributes.classList.remove('ad-form--disabled');
+    mapsAttributes.classList.remove('map__filters--disabled');
+    fieldsetElements.forEach ((fieldsetElement) => {
+      fieldsetElement.removeAttribute('disabled', false);
+    });
+    selectElements.forEach ((selectElement) => {
+      selectElement.removeAttribute('disabled', false);
+    });
+  }
+};
+
+export {toggleFormState};
 
 const adForm = document.querySelector('.ad-form');
 const inputTitle = adForm.querySelector('#title');
@@ -54,9 +57,9 @@ const capacityOfRoomsAdForm = adForm.querySelector('#capacity');
 export const getValidateCapacityAndRooms = () => {
   const rooms = Number(numberOfRoomsAdForm.value);
   const guests = Number(capacityOfRoomsAdForm.value);
-  if (rooms === 100 && guests !== 0) {
+  if (rooms === MAX_APARTMENTS && guests !== MIN_VISITORS) {
     numberOfRoomsAdForm.setCustomValidity('Такое колличество комнат не для гостей');
-  } else if (rooms !== 100 && guests === 0) {
+  } else if (rooms !== MAX_APARTMENTS && guests === MIN_VISITORS) {
     numberOfRoomsAdForm.setCustomValidity('Не для гостей" можно выбрать только 100 комнат');
   } else if (rooms < guests) {
     numberOfRoomsAdForm.setCustomValidity('Колличество комнат должно быть больше или равно колличеству гостей');
@@ -78,38 +81,3 @@ export const generateValidityError = function (evt) {
   }
 };
 
-const housingTypeAdForm = adForm.querySelector('#type');
-const priceAdForm = adForm.querySelector('#price');
-const minHousingPrice = {
-  bungalow: 0,
-  flat: 1000,
-  house: 5000,
-  hotel: 3000,
-  palace: 10000,
-};
-
-const validatePriceRoom = () => {
-  const minPrice = minHousingPrice[housingTypeAdForm.value];
-  priceAdForm.placeholder = minPrice;
-  if (priceAdForm.value < minPrice) {
-    priceAdForm.setCustomValidity(`Минимальная цена данного типа жилья ${minPrice} рублей`);
-  } else {
-    priceAdForm.setCustomValidity('');
-  }
-};
-
-priceAdForm.addEventListener('input', validatePriceRoom);
-housingTypeAdForm.addEventListener('change', validatePriceRoom);
-
-const timeinAdForm = adForm.querySelector('#timein');
-const timeoutAdForm = adForm.querySelector('#timeout');
-
-const validateTime = (evt) => {
-  if (evt.target.name === 'timeout' || evt.target.name === 'timein') {
-    timeinAdForm.value = evt.target.value;
-    timeoutAdForm.value = evt.target.value;
-  }
-};
-
-timeinAdForm.addEventListener('change', validateTime);
-timeoutAdForm.addEventListener('change', validateTime);

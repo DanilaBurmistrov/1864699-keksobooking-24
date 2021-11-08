102ec49678e1e0c29507edf9849d0cee164dc528
DanilaBurmistrov <Burmistrov.danila2016@yandex.ru>
2021-11-08 23:17:28 +0300

    общение с сервером

---
+++ js/fetch.js
@@ -0,0 +1,39 @@
+import {showAlert} from './util.js';
+
+const getData = (onSuccess) => {
+  fetch('https://24.javascript.pages.academy/keksobooking/data')
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      throw new Error(`${response.status} - ${response.statusText}`);
+    })
+    .then((response) => response.json())
+    .then((ad) => {
+      onSuccess(ad);
+    })
+    .catch(() => {
+      showAlert('Ошибка загрузки данных с сервера : - (');
+    });
+};
+
+const sendData = (onSuccess, onFail, body) => {
+  fetch('https://24.javascript.pages.academy/keksobooking/data',
+    {
+      method: 'POST',
+      body,
+    })
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      }
+      else {
+        onFail();
+      }
+    })
+    .catch(() => {
+      onFail();
+    });
+};
+
+export {getData, sendData};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {getMapPoints, resetAdFormLocation} from './map.js';
 import {generateDataArray} from './data.js';
-
+import './form.js';
 
 const dataArray = generateDataArray();
 getMapPoints(dataArray);
